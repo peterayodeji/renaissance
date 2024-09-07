b29e312e417f5b8f3f4ff28427c1e022fea84551
Peter Ayodeji <pb.ayodeji@gmail.com>
2024-09-07 18:51:38 +0100

    animate navigation links to slide-in

--- src/ui/NavLinksList.jsx
+++ src/ui/NavLinksList.jsx
@@ -1,5 +1,7 @@
 function NavLinksList({ children }) {
-  return <ul className="mb-8 space-y-4 px-8">{children}</ul>;
+  return (
+    <ul className="mb-8 animate-SlideInLong space-y-4 px-8">{children}</ul>
+  );
 }
 
 export default NavLinksList;

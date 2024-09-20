beae97d03ef74436614ccf016a40588d7211c1d0
Peter Ayodeji <pb.ayodeji@gmail.com>
2024-09-20 17:41:30 +0100

    add more content and styling to PageNotFound page

--- src/ui/PageNotFound.jsx
+++ src/ui/PageNotFound.jsx
@@ -1,7 +1,14 @@
 function PageNotFound() {
   return (
-    <div className="bg-blue-40 flex flex-grow items-center justify-center text-2xl md:text-3xl">
-      <p className="mb-14 md:mb-16 2xl:mb-20">PAGE NOT FOUND!</p>
+    <div className="bg-blue-40 flex flex-grow items-center justify-center">
+      <div className="mb-14 text-center md:mb-16 2xl:mb-20">
+        <h3 className="mb-6 text-2xl font-semibold md:text-3xl">
+          PAGE NOT FOUND!
+        </h3>
+        <p>
+          The page you&#39;re looking for <br /> is currently unavailable.
+        </p>
+      </div>
     </div>
   );
 }

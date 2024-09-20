92c261d575d2ac0715e44442a924325ea5b5e380
Peter Ayodeji <pb.ayodeji@gmail.com>
2024-09-20 15:14:06 +0100

    include filler in main section to balance structure

--- src/ui/Hero.jsx
+++ src/ui/Hero.jsx
@@ -17,28 +17,27 @@ function Hero() {
 
   return (
     <>
-      <div className="relative hidden h-[100vh] md:block">
-        <div
-          style={{ backgroundImage: `url(${imageUrl.des})` }}
-          className={`absolute inset-0 mt-16 h-[calc(100vh-4rem)] bg-stone-400 bg-cover bg-center bg-no-repeat 2xl:mt-20 2xl:h-[calc(100vh-5rem)]`}
-        >
-          <div className="absolute left-[50%] top-[50%] space-y-6 text-white [transform:translate(-50%,20%)]">
-            <h1 className="text-5xl tracking-wide 2xl:text-6xl">Aura Line</h1>
-            <p className="outlined-text text-base font-medium 2xl:text-lg">
-              DISCOVER YOUR SIGNATURE STYLE
-            </p>
-            <a
-              href="#"
-              className="inline-block bg-gray-950 px-10 py-2 text-white 2xl:text-lg"
-            >
-              Shop Now
-            </a>
-          </div>
+      {/* // # DESKTOP HERO  */}
+      <div
+        style={{ backgroundImage: `url(${imageUrl.des})` }}
+        className={`relative hidden h-[calc(100vh-4rem)] bg-stone-400 bg-cover bg-center bg-no-repeat md:block 2xl:h-[calc(100vh-5rem)]`}
+      >
+        <div className="absolute left-[50%] top-[50%] space-y-6 text-white [transform:translate(-50%,20%)]">
+          <h1 className="text-5xl tracking-wide 2xl:text-6xl">Aura Line</h1>
+          <p className="outlined-text text-base font-medium 2xl:text-lg">
+            DISCOVER YOUR SIGNATURE STYLE
+          </p>
+          <a
+            href="#"
+            className="inline-block bg-gray-950 px-10 py-2 text-white 2xl:text-lg"
+          >
+            Shop Now
+          </a>
         </div>
       </div>
 
-      {/* // #MOBILE HERO  */}
-      <div className="space-y-4 py-14 text-center md:hidden">
+      {/* // # MOBILE HERO  */}
+      <div className="space-y-4 pb-14 text-center md:hidden">
         <img src={imageUrl.mob} alt="Header" />
         <h1 className="text-xl tracking-wider sm:text-2xl">Aura Line</h1>
         <p className="text-sm font-medium">DISCOVER YOUR SIGNATURE STYLE</p>
--- src/ui/Main.jsx
+++ src/ui/Main.jsx
@@ -1,5 +1,10 @@
 function Main({ children }) {
-  return <main className="min-h-screen">{children}</main>;
+  return (
+    <main className="min-h-screen">
+      <div className={`h-14 md:h-16 2xl:h-20`}></div>
+      {children}
+    </main>
+  );
 }
 
 export default Main;

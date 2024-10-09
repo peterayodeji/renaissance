b5e9dee60dea8f837c6a29a29c4501d10aaa8861
Peter Ayodeji <pb.ayodeji@gmail.com>
2024-10-10 00:11:45 +0100

    update environment configuration

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
-export const supabaseUrl = 'https://mlzusuqzsnmeuplbwwlh.supabase.co';
-const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1senVzdXF6c25tZXVwbGJ3d2xoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY5NjcxNzksImV4cCI6MjA0MjU0MzE3OX0.nFHUnJ-gHrTe1ZEfHF7BNVL4I_BXt4N2XdOpxJ103Jk';
+const supabaseUrl = import.meta.env.VITE_API_URL;
+const supabaseKey = import.meta.env.VITE_API_KEY;
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;

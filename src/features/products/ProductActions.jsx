7d5fb8192f74c89d9808dbb86d14e7571225062f
Peter Ayodeji <pb.ayodeji@gmail.com>
2024-10-09 19:13:19 +0100

    distinguish between product action buttons

--- src/features/products/ProductActions.jsx
+++ src/features/products/ProductActions.jsx
@@ -6,7 +6,7 @@ function ProductActions({ product }) {
 
   return (
     <article className="bg-blue-20 flex items-center lg:sticky lg:bottom-0 lg:h-[calc(100vh-4rem)] lg:pr-8 xl:px-10 2xl:h-[calc(100vh-5rem)] 2xl:px-12">
-      <div className="bg-cyan-20 w-full space-y-4 text-sm">
+      <form className="bg-cyan-20 w-full space-y-4 text-sm">
         <div className="flex justify-between">
           <h3 className="w-40 uppercase lg:hidden">{product.name}</h3>
           <p>${price} USD</p>
@@ -15,6 +15,7 @@ function ProductActions({ product }) {
         <select
           value={size}
           onChange={e => setSize(e.target.value)}
+          required
           className="w-full border border-black bg-white px-1 py-2 shadow-sm focus:outline-none"
         >
           <option value="" disabled>
@@ -35,17 +36,23 @@ function ProductActions({ product }) {
         </select>
 
         <div className="flex justify-between">
-          <button className="flex-1 bg-black py-2 uppercase text-white">
+          <button
+            type="submit"
+            className="flex-1 bg-black py-2 uppercase text-white"
+          >
             Add To Bag
           </button>
-          <button className="px-4 py-2 uppercase sm:flex-1 sm:px-0">
-            <span className="hidden sm:inline-block">Add To Wishlist</span>
-            <img src="/heart.svg" alt="Favourite" className="sm:hidden" />
+          <button
+            type="button"
+            className="px-4 py-2 uppercase md:flex-1 md:px-0"
+          >
+            <span className="hidden md:inline-block">Add To Wishlist</span>
+            <img src="/heart.svg" alt="Favourite" className="md:hidden" />
           </button>
         </div>
 
         <p>{model}</p>
-      </div>
+      </form>
     </article>
   );
 }

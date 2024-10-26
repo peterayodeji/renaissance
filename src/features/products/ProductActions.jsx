1e7bacbcef09948533e7c867fb9313693da8b08f
Peter Ayodeji <pb.ayodeji@gmail.com>
2024-10-26 18:34:36 +0100

    implement cart state and functioning

--- src/features/products/ProductActions.jsx
+++ src/features/products/ProductActions.jsx
@@ -1,31 +1,69 @@
 import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addItem, getCartItemById } from '../cart/cartSlice';
 
 function ProductActions({ product }) {
-  const [size, setSize] = useState('');
-  const { price, sizes, model } = product;
+  const dispatch = useDispatch();
+  const [currentSize, setCurrentSize] = useState({ value: '', quantity: null });
+
+  const { id, name, description, color, price, sizes, model } = product;
+  const cartItemId = id + currentSize.value + color;
+
+  // Get cartItem from cart that matches (id, size, color)
+  const cartItem = useSelector(getCartItemById(cartItemId));
+
+  function handleInputChange(e) {
+    const value = e.target.value;
+    const quantity = +e.target.selectedOptions[0].dataset.quantity;
+
+    setCurrentSize(size => ({ ...size, value, quantity }));
+  }
+
+  function handleAddToCart(e) {
+    e.preventDefault();
+
+    const newItem = {
+      itemId: cartItemId,
+      name,
+      description,
+      color,
+      size: currentSize.value,
+      quantity: 1,
+      maxQuantity: currentSize.quantity,
+      unitPrice: price,
+      totalPrice: price * 1,
+    };
+
+    dispatch(addItem(newItem));
+  }
 
   return (
     <article className="bg-blue-20 flex items-center lg:sticky lg:bottom-0 lg:h-[calc(100vh-4rem)] lg:pr-8 xl:px-10 2xl:h-[calc(100vh-5rem)] 2xl:px-12">
-      <form className="bg-cyan-20 w-full space-y-4 text-sm">
+      <form
+        onSubmit={handleAddToCart}
+        className="bg-cyan-20 w-full space-y-4 text-sm"
+      >
         <div className="flex justify-between">
           <h3 className="w-40 uppercase lg:hidden">{product.name}</h3>
           <p>${price} USD</p>
         </div>
 
         <select
-          value={size}
-          onChange={e => setSize(e.target.value)}
+          value={currentSize.value}
+          onChange={handleInputChange}
           required
           className="w-full border border-black bg-white px-1 py-2 shadow-sm focus:outline-none"
         >
           <option value="" disabled>
             SELECT A SIZE
           </option>
+
           {sizes.map(size => (
             <option
               key={size.value}
               value={size.value}
               disabled={size.quantity < 1}
+              data-quantity={size.quantity}
             >
               {size.label} -{' '}
               {size.quantity < 1
@@ -38,10 +76,14 @@ function ProductActions({ product }) {
         <div className="flex justify-between">
           <button
             type="submit"
-            className="flex-1 bg-black py-2 uppercase text-white"
+            disabled={
+              cartItem ? cartItem.quantity >= currentSize.quantity : false
+            }
+            className="flex-1 bg-black py-2 uppercase text-white disabled:cursor-not-allowed disabled:bg-stone-900"
           >
             Add To Bag
           </button>
+
           <button
             type="button"
             className="px-4 py-2 uppercase md:flex-1 md:px-0"

a8bae131dcdb7ae9ef3fab13a22ec08c92a19c7b
Peter Ayodeji <pb.ayodeji@gmail.com>
2024-11-06 19:37:41 +0100

    fix header logo centering

--- src/ui/MobileHeader.jsx
+++ src/ui/MobileHeader.jsx
@@ -11,8 +11,8 @@ function MobileHeader() {
   }
 
   return (
-    <div className="sticky top-0 z-20 flex h-14 w-full items-center justify-between bg-white px-4 sm:px-8 md:h-16 md:px-12">
-      <div className="flex flex-row items-center gap-x-2 md:gap-x-4">
+    <div className="sticky top-0 z-20 flex h-14 w-full items-center bg-white px-4 sm:px-8 md:h-16 md:px-12">
+      <div className="flex flex-1 flex-row items-center gap-x-2 md:gap-x-4">
         <img src="/menu.svg" alt="Search" onClick={() => setIsOpen(true)} />
 
         <Search />
@@ -20,7 +20,7 @@ function MobileHeader() {
 
       <h1 className="text-xl font-bold text-black sm:text-2xl">RENAISSANCE</h1>
 
-      <div className="flex flex-row items-center">
+      <div className="flex flex-1 flex-row items-center justify-end">
         <img src="/shopping-bag.svg" alt="Shopping Bag" />
         <span className="text-sm font-bold">(1)</span>
       </div>

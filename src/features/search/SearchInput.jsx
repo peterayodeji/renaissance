9032b2f0e02ff8587ddc26c24f88658a2362494b
Peter Ayodeji <pb.ayodeji@gmail.com>
2025-02-15 13:35:02 +0100

    dynamically set focus on search input

--- src/features/search/SearchInput.jsx
+++ src/features/search/SearchInput.jsx
@@ -1,10 +1,26 @@
+import { useEffect, useRef } from 'react';
 import { convertTitleCase } from '../../utils/helpers';
 
 function SearchInput({ searchInput, setSearchInput, category }) {
+  const inputEl = useRef();
+
+  useEffect(
+    function () {
+      inputEl.current.focus();
+    },
+    [category],
+  );
+
+  function clearInput() {
+    setSearchInput('');
+    inputEl.current.focus();
+  }
+
   return (
     <div className="relative mb-4">
       <input
         value={searchInput}
+        ref={inputEl}
         placeholder={`Search ${convertTitleCase(category)}`}
         onChange={e => setSearchInput(e.target.value)}
         className="w-full border px-4 py-3 text-sm outline-none placeholder:text-sm placeholder:text-inherit"
@@ -17,7 +33,7 @@ function SearchInput({ searchInput, setSearchInput, category }) {
       )}
 
       {searchInput && (
-        <SearchIcon onClick={() => setSearchInput('')}>
+        <SearchIcon onClick={clearInput}>
           <img src="/x.svg" alt="Cancel" className="w-5" />
         </SearchIcon>
       )}

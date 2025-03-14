5bb95970e35c8e120f97c890bc6ca317618c014c
Peter Ayodeji <pb.ayodeji@gmail.com>
2025-03-14 13:13:29 +0100

    include account addresses in the account board

--- src/features/account/AccountAddresses.jsx
+++ src/features/account/AccountAddresses.jsx
@@ -1,5 +1,148 @@
+import { useForm } from 'react-hook-form';
+import FormRow from '../../ui/FormRow';
+
 function AccountAddresses() {
-  return <h2 className="text-xl font-semibold">Account Addresses </h2>;
+  const { register, formState } = useForm();
+  const { errors } = formState;
+
+  return (
+    <form className="bg-yellow-30">
+      <h2 className="bg-blue-20 mb-10 text-center text-3xl font-semibold">
+        Addresses
+      </h2>
+
+      <div className="space-y-4">
+        <div className="flex flex-col gap-x-2 gap-y-4 sm:flex-row md:flex-col lg:flex-row">
+          <FormRow label="First Name" error={errors?.firstName?.message}>
+            <input
+              type="text"
+              id="firstName"
+              {...register('firstName', {
+                required: 'This field is required',
+              })}
+              className="input"
+            />
+          </FormRow>
+
+          <FormRow label="Last Name" error={errors?.lastName?.message}>
+            <input
+              type="text"
+              id="lastName"
+              {...register('lastName', {
+                required: 'This field is required',
+              })}
+              className="input"
+            />
+          </FormRow>
+        </div>
+
+        <div className="flex flex-col gap-x-2 gap-y-4 sm:flex-row md:flex-col lg:flex-row">
+          <FormRow
+            label="Street Address"
+            error={errors?.streetAddress?.message}
+          >
+            <input
+              type="text"
+              id="streetAddress"
+              {...register('streetAddress', {
+                required: 'This field is required',
+              })}
+              className="input"
+            />
+          </FormRow>
+
+          <FormRow label="Company (Optional)" error={errors?.company?.message}>
+            <input
+              type="text"
+              id="company"
+              {...register('company')}
+              className="input"
+            />
+          </FormRow>
+        </div>
+
+        <div className="flex flex-col gap-x-2 gap-y-4 sm:flex-row md:flex-col lg:flex-row">
+          <FormRow label="City" error={errors?.city?.message}>
+            <input
+              type="text"
+              id="city"
+              {...register('city', {
+                required: 'This field is required',
+              })}
+              className="input"
+            />
+          </FormRow>
+
+          <FormRow label="ZIP Or Postal Code" error={errors?.zip?.message}>
+            <input
+              type="text"
+              id="zip"
+              {...register('zip', {
+                required: 'This field is required',
+              })}
+              className="input w-44"
+            />
+          </FormRow>
+        </div>
+
+        {/* <div className="flex flex-col gap-x-2 gap-y-4 sm:flex-row md:flex-col lg:flex-row">
+          <FormRow label="Country/Region" error={errors?.country?.message}>
+            <select
+              id="country"
+              {...register('country', { required: 'Country is required' })}
+              value={selectedCountry}
+              onChange={handleCountryChange}
+              className="input"
+            >
+              <option value="">Select Country</option>
+              {countryOptions.map(({ countryCode, country }) => (
+                <option key={countryCode} value={countryCode}>
+                  {truncateText(country, 30)}
+                </option>
+              ))}
+            </select>
+          </FormRow>
+
+          <FormRow label="State/Province" error={errors?.state?.message}>
+            <select
+              id="state"
+              {...register('state', { required: 'State is required' })}
+              disabled={!selectedCountry}
+              className="input disabled:cursor-not-allowed"
+            >
+              {stateOptions.map(([state, stateCode]) => (
+                <option key={stateCode} value={state}>
+                  {truncateText(state, 30)}
+                </option>
+              ))}
+            </select>
+          </FormRow>
+        </div> */}
+
+        <FormRow label="Phone" error={errors?.zip?.message}>
+          <input
+            // inputMode="numeric"
+            // pattern="[0-9 ]*"
+            // onKeyDown={preventNonNumeric}
+            type="text"
+            id="phone"
+            {...register('phone', {
+              required: 'This field is required',
+            })}
+            className="input w-44"
+          />
+        </FormRow>
+      </div>
+
+      <button
+        type="submit"
+        // disabled={isLoading}
+        className="mt-8 w-full bg-black py-4 font-medium tracking-wider text-white disabled:opacity-5"
+      >
+        <span className="undeline">Save Changes</span>
+      </button>
+    </form>
+  );
 }
 
 export default AccountAddresses;
--- src/features/account/AccountLayout.jsx
+++ src/features/account/AccountLayout.jsx
@@ -2,9 +2,9 @@ import { Link, Outlet } from 'react-router-dom';
 
 function AccountLayout() {
   return (
-    <div className="flex grow bg-stone-400 px-8 text-sm lg:items-end xl:px-10 2xl:px-12">
+    <div className="bg-stone-40 flex grow px-8 text-sm lg:items-end xl:px-10 2xl:px-12">
       {/* Sidebar */}
-      <aside className="hiddenn bg-pink-80 flex-1 lg:sticky lg:bottom-0 lg:h-[calc(100vh-4rem)]">
+      <aside className="hiddenn bg-pink-80 flex-1 lg:sticky lg:bottom-0 lg:h-[calc(100vh-4rem)] 2xl:h-[calc(100vh-5rem)]">
         <nav className="mt-14">
           <h3 className="mb-6 font-semibold">Account</h3>
 
@@ -29,12 +29,12 @@ function AccountLayout() {
       </aside>
 
       {/* Main Content */}
-      {/* Review height later for the 3 - all screen sizes */}
-      <main className="bg-blue-60 w-full max-w-sm pb-14 pt-12 lg:min-h-[calc(100vh-4rem)] xl:max-w-md">
+      <main className="bg-blue-60 w-full max-w-sm pb-14 pt-12 lg:min-h-[calc(100vh-4rem)] xl:max-w-md 2xl:min-h-[calc(100vh-5rem)]">
         <Outlet />
       </main>
 
-      <aside className="bg-teal-30 flex-1 lg:sticky lg:bottom-0 lg:h-[calc(100vh-4rem)]">
+      {/* Filler */}
+      <aside className="bg-teal-30 flex-1 lg:sticky lg:bottom-0 lg:h-[calc(100vh-4rem)] 2xl:h-[calc(100vh-5rem)]">
         &nbsp;
       </aside>
     </div>
